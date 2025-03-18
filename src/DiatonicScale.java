bbab05dae5c562e89f88c8adbe389545fec24141
luca <19bulavitchi_l@bexleygs.co.uk>
2025-03-18 20:47:03 +0000

    vague/rough idea developing

--- src/DiatonicScale.java
+++ src/DiatonicScale.java
@@ -3,59 +3,50 @@ import java.util.ArrayList;
 public class DiatonicScale {
     private Key key;
     private int[] octRange;
-    private String tonality;
-    //private String[] augmentations;
-    //private ArrayList<String> augArr;
+    private Tonality tonality;
 
-    public void setKey(Key key){
-        this.key = key;
-    }
+    private int scaleLength;
+    private String[] NOTES = {"A","B","C","D","E","F","G"};
+    private String[] organisedScale;
+
+    public void setKey(Key key){this.key = key;}
     public void setOctRange(int[] octRange){
         this.octRange = octRange;
     }
-    public void setTonality(String tonality){
+    public void setTonality(Tonality tonality){
         this.tonality = tonality;
     }
-    //public void setAugmentations(String[] augmentations) {
-    //    this.augmentations = augmentations;
-   // }
-
-    //public void setAugArr(ArrayList<String> augArr){
-    //    this.augArr = augArr;
-    //}
 
     public void display() {
         System.out.println("Key: " + key + " " + tonality);
         System.out.println("Octave range: " + octRange[0] + " to " + octRange[1]);
-
-
-
-
-
-
-        //for (int i = 0; i <= augmentations.length; i++) {
-        //    System.out.println("Augmentation: "+augmentations[i]);
-        //}
     }
 
-    //public boolean isValid(){
-    //    boolean keyV = false;
-    //    boolean octRangeV = false;
-    //    boolean tonalityV = false;
-    //    boolean augmentationsV = false;
-    //    if((keyV = true) && (octRangeV = true) && (tonalityV = true) && (augmentationsV = true)){
-    //        System.out.println("valid");
-    //        return true;
-    //    }
-    //    else{
-    //        System.out.println("invalid");
-    //        return false;
-    //    }
-    //}
+    //finds length of scale
+    public void numOfNotes(){
+        scaleLength = (octRange[1] - octRange[0]) * 7 + 1;
+            System.out.println("Number of notes: " + scaleLength);
+    }
 
+    //creates a list of the scale ie A1 B1 C1 ... G5 A6
+    public void allNotes(){
+        //organises scale so that tonic/key the is requested is first
+        organisedScale[0] = key.toString();
+        System.out.println(NOTES.indexOf(organisedScale[0]));
+
+
+
+        //outputs from start of organised scale to end
+        for(int i =0;i<(octRange[1]-octRange[0]);i++) {
+            for(int j=0;j<7;j++){
+                System.out.println(NOTES[j] + (i+octRange[0]));
+            }
+        }
+        System.out.println(key.toString() + octRange[1]);
+    }
 
 
     //make a class that takes the octave range start and end
-    //and then saves all of the notes (ie C2 D2 E2 ... C3)
+    //and then saves all the notes (ie C2 D2 E2 ... C3)
 }
 
--- src/Main.java
+++ src/Main.java
@@ -1,52 +1,35 @@
 import java.util.ArrayList;
 
 public class Main {
-
-
-
     public static void main(String[] args) {
-        Key key = Key.A;
-
+        Key key = Key.B;
         int[] octRange = {2,6};
-        String tonality = "Minor";
-        //String[] augmentations = {"4-","7+"};
-        //ArrayList<String> augArr;
-        //augArr.add("4+");
-        //augArr.add("7-");
-
+        Tonality tonality = Tonality.Minor;
 
         DiatonicScale test = new DiatonicScale();
         test.setKey(key);
         test.setOctRange(octRange);
         test.setTonality(tonality);
-        // test.setAugmentations(augmentations);
         test.display();
-
-        /////////////
+        test.numOfNotes();
+        test.allNotes();
 
         CircleOfFifths x = new CircleOfFifths();
-        System.out.println(x.getTone(Key.A, Tonality.MINOR));
+        System.out.println(x.getTone(Key.A, Tonality.Minor));
 
     }
 }
 
 class Tone {
     private String description;
-
     public Tone(String description) {
         this.description  = description;
     }
-
     @Override
     public String toString() {
         return description ;
     }
 }
 
-enum Key {
-    A, B, C, D, E, F, G
-}
-
-enum Tonality {
-    MINOR
-}
+enum Key {A, B, C, D, E, F, G, a, b, c, d, e, f, g}
+enum Tonality {Major, Minor, Ionian, Dorian, Phrygian, Lydian, Mixolydian, Aeolian, Locrian}
